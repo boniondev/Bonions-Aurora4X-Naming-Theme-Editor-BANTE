e5a1d710f7efb831ccf56b0724bd08333eeaf911
Bonion-Laptop <55188452+boniondev@users.noreply.github.com>
2025-09-23 10:29:53 +0200

    User proper naming style for constants

--- bante.py
+++ bante.py
@@ -22,56 +22,56 @@ def connect_ro(path : str) -> sqlite3.Connection:
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(
         prog="py[thon] bante.py",
         description="Bonion's Aurora4X Naming Theme Editor",
         epilog="Backup your DB before using this tool!",
+    PARSER = argparse.ArgumentParser(
     )
-    mutualexcgroup = parser.add_mutually_exclusive_group(required = True)
-    parser.add_argument(
+    MUTUALEXCGROUP = PARSER.add_mutually_exclusive_group(required = True)
+    PARSER.add_argument(
         "-p", "--path",
         help =  "UNIX style absolute or relative path to Aurora.db " +
                 "(Looks inside current directory if unspecified)",
         required = False
     )
-    mutualexcgroup.add_argument(
+    MUTUALEXCGROUP.add_argument(
         "-l", "--list", help = "List all naming theme", action ="store_true", dest = "list"
     )
-    mutualexcgroup.add_argument(
+    MUTUALEXCGROUP.add_argument(
         "-ln", "--list-names",
         help = "List all names from a given ThemeID",
         action = "store", dest = "themeid", type = int
     )
-    mutualexcgroup.add_argument(
+    MUTUALEXCGROUP.add_argument(
         "-dn", "--delete-name",
         help = "Delete name(s) from a given name theme." \
                 " Insert the ThemeID, and then the rows you wish to delete, separated by spaces",
         action = "store", nargs = "+", metavar = ("THEMEID", "DELETIONS"), dest = "deletions"
     )
 
-    args = parser.parse_args()
-    if args.list:
-        conn = connect_ro(args.path)
-        cur = conn.cursor()
+    ARGS = PARSER.parse_args()
+    if ARGS.list:
+        CONN = connect_ro(ARGS.path)
+        cur = CONN.cursor()
         cur.execute("SELECT * FROM DIM_NamingThemeTypes")
         rows = cur.fetchall()
         print("ThemeID | Description")
         print("---------------------")
         for row in rows:
             print(f"{row[0]} | {row[1]}")
-        conn.close()
-    elif args.themeid:
-        conn = connect_ro(args.path)
-        cur = conn.cursor()
-        cur.execute(f"SELECT Name FROM DIM_NamingTheme WHERE NameThemeID == '{args.themeid}'")
+        CONN.close()
+    elif ARGS.themeid:
+        CONN = connect_ro(ARGS.path)
+        cur = CONN.cursor()
+        cur.execute(f"SELECT Name FROM DIM_NamingTheme WHERE NameThemeID == '{ARGS.themeid}'")
         rows = cur.fetchall()
-        num = 0
+        NUM = 0
         for row in rows:
-            print(f"{num}|{row[0]}")
-            num = num + 1
-        conn.close()
-    elif args.deletions:
-        if len(args.deletions) < 2:
+            print(f"{NUM}|{row[0]}")
+            NUM = NUM + 1
+        CONN.close()
+    elif ARGS.deletions:
+        if len(ARGS.deletions) < 2:
             print("ThemeID was provided, but no rows to delete were given.")
             print("Deleting nothing!")
             sys.exit(0)
